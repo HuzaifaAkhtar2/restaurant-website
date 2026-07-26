7a5735516c80c0c11df0c7bf2e88355095eaac42
Huzaifa Akhtar <huzaifaakh@outlook.com>
2026-07-26 10:15:21 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// ==========================
+// MOBILE NAVIGATION
+// ==========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuToggle) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
+
+// ==========================
+// CLOSE MOBILE MENU
+// ==========================
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
+// ==========================
+// CURRENT YEAR FOOTER
+// ==========================
+
+const year = document.querySelector(".year");
+
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
+
+// ==========================
+// SCROLL REVEAL ANIMATION
+// ==========================
+
+const revealElements = document.querySelectorAll(
+  ".about, .card, .hours-box, .contact-grid, iframe, .menu-card",
+);
+
+const revealOnScroll = () => {
+  revealElements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
+
+// ==========================
+// SMOOTH SCROLL
+// ==========================
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
